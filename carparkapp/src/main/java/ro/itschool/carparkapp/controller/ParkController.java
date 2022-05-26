bb529648d76e21da109bded7874b26e1e881fe4c
todincaandrei <todinca15@gmail.com>
2022-05-26 15:07:35 +0300

    Add-Park Html and methods in controller

--- carparkapp/src/main/java/ro/itschool/carparkapp/controller/ParkController.java
+++ carparkapp/src/main/java/ro/itschool/carparkapp/controller/ParkController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import ro.itschool.carparkapp.entity.ParkModel;
 import ro.itschool.carparkapp.service.ParkService;
 
@@ -20,6 +21,18 @@ public class ParkController {
         List<ParkModel> parkModelsList = parkService.getPark();
         myModel.addAttribute("parks",parkModelsList);
         return "parks";
+    }
+
+    @GetMapping("add-park")
+    public String addParkPage(Model model){
+        model.addAttribute("park",new ParkModel());
+        return "add-park";
+    }
+
+    @PostMapping("add-new-park")
+    public String addNewPark(ParkModel parkModel){
+        parkService.addPark(parkModel);
+        return "redirect:/view-parks";
     }
 
 }
--- carparkapp/src/main/java/ro/itschool/carparkapp/service/ParkService.java
+++ carparkapp/src/main/java/ro/itschool/carparkapp/service/ParkService.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Component
 public class ParkService {
 
-//    add get delete
+
 
     @Autowired
     private ParkRepository parkRepository;
