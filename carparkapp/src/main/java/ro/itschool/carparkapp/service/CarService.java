c7ccb7ded783d62f52c8a226cd76349db685b001
todincaandrei <todinca15@gmail.com>
2022-06-03 14:27:11 +0300

    Cars edit and delete added and working

--- carparkapp/src/main/java/ro/itschool/carparkapp/service/CarService.java
+++ carparkapp/src/main/java/ro/itschool/carparkapp/service/CarService.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ro.itschool.carparkapp.entity.CarModel;
 import ro.itschool.carparkapp.repository.CarRepository;
+import ro.itschool.carparkapp.service.exception.CarNotFoundException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CarService {
@@ -22,5 +24,31 @@ public class CarService {
     }
 
     public void addCar(CarModel carModel) {carRepository.save(carModel);
+    }
+
+    public CarModel getCarById(int carId) throws CarNotFoundException {
+        Optional<CarModel> optionalCarModel = carRepository.findById(carId);
+
+        if (optionalCarModel.isEmpty()){
+            throw new CarNotFoundException("Car with id: " + carId + " does not exists");
+        }
+        CarModel carModel = optionalCarModel.get();
+
+        return carModel;
+    }
+
+    public void updateCar(CarModel modifiedCar) throws CarNotFoundException{
+        CarModel existingCar = getCarById(modifiedCar.getId());
+
+        existingCar.setBrand(modifiedCar.getBrand());
+        existingCar.setColour(modifiedCar.getColour());
+        existingCar.setModel(modifiedCar.getModel());
+        existingCar.setTransmission(modifiedCar.getTransmission());
+        existingCar.setYearOfFabrication(modifiedCar.getYearOfFabrication());
+
+        carRepository.save(existingCar);
+    }
+
+    public void removeCar(int carId) {carRepository.deleteById(carId);
     }
 }
---
+++ carparkapp/src/main/java/ro/itschool/carparkapp/service/exception/CarNotFoundException.java
@@ -0,0 +1,6 @@
+package ro.itschool.carparkapp.service.exception;
+
+public class CarNotFoundException extends Exception {
+
+    public CarNotFoundException(String message){super(message);}
+}
